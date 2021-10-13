cb984f39c1cdd6ac046f396d8472a80eb309eb05
Colin-J-Wood <w012764k@student.staffs.ac.uk>
2021-10-13 11:36:05 +0100

    finishing variable practice

--- hello-world/HelloWorld/Week2/Week2.cpp
+++ hello-world/HelloWorld/Week2/Week2.cpp
@@ -12,6 +12,35 @@ int main()
     std::cout << "Size of float: " << sizeof(float) << " bytes" << std::endl;
     std::cout << "Size of double: " << sizeof(double) << " bytes" << std::endl;
     std::cout << "Size of wchar_t: " << sizeof(wchar_t) << " bytes" << std::endl;
+
+    typedef int date;
+    date currentDate;
+
+    enum Month
+    {
+        JANUARY = 1,
+        FEBRUARY,
+        MARCH,
+        APRIL,
+        MAY,
+        JUNE,
+        JULY,
+        AUGUST,
+        SEPTEMBER,
+        OCTOBER,
+        NOVEMBER,
+        DECEMBER
+    };
+
+    Month currentMonth;
+
+    int myNum = 5;
+    myNum = 10;
+
+    double myNum1 = 4.0f, myNum2 = 4.32f, myNum3;
+
+    const float kUniversalGravity = 6.67408f;
+    const float kEarthGravity = 9.8f;
     
     return 0;
 }
