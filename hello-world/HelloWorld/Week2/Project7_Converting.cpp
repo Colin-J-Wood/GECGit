125a14bcce8edc2bad1c3415270116b184220657
Colin-J-Wood <w012764k@student.staffs.ac.uk>
2021-10-13 12:30:05 +0100

    comment out other main functions
    In order to select which one to compile, uncomment each.  I might be able to stick each of these in seperate functions and shove it all in one file, depending on the marker's wishes on visibility.  I might also need to split these files back up into their own projects.  We'll see.

--- hello-world/HelloWorld/Week2/Project7_Converting.cpp
+++ hello-world/HelloWorld/Week2/Project7_Converting.cpp
@@ -1,7 +1,14 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	//no need for higher precision.
+//	//set the value when declaring.
+//	float fahrenheit = 95.0f;
+//
+//	//you can use formulas whilst declaring your variable for initialization.  This saves lines of code.
+//	float celsius = (fahrenheit - 32) * 0.5556;
+//	
+//	cout << fahrenheit << "F is equal to " << celsius << "C." << endl;
+//}
--- hello-world/HelloWorld/Week2_UserDefinedVars/Project4_UserDefinedVariables.cpp
+++ hello-world/HelloWorld/Week2_UserDefinedVars/Project4_UserDefinedVariables.cpp
@@ -1,25 +1,25 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    typedef int health;
-    enum Weapons
-    {
-        SWORD = 1,
-        DAGGER,
-        MACE,
-        TWIN_SWORDS,
-        SAMURAI,
-        WIZARD_STAFF,
-        FIRE_POTION,
-        ICE_BLADE,
-        SMALL_KNIFE
-    };
-
-    health myHealth = 13000;
-    Weapons myWeapon = SAMURAI;
-
-    cout << "My current health is: " << myHealth << endl;
-    cout << "And the ID of my weapon of choice is: " << myWeapon << endl;
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    typedef int health;
+//    enum Weapons
+//    {
+//        SWORD = 1,
+//        DAGGER,
+//        MACE,
+//        TWIN_SWORDS,
+//        SAMURAI,
+//        WIZARD_STAFF,
+//        FIRE_POTION,
+//        ICE_BLADE,
+//        SMALL_KNIFE
+//    };
+//
+//    health myHealth = 13000;
+//    Weapons myWeapon = SAMURAI;
+//
+//    cout << "My current health is: " << myHealth << endl;
+//    cout << "And the ID of my weapon of choice is: " << myWeapon << endl;
+//}
