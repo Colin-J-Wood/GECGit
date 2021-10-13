051a87f6eddf2b0e200dd171aa7de83f6d2b8ab6
Colin-J-Wood <w012764k@student.staffs.ac.uk>
2021-10-13 12:04:45 +0100

    fix project names along with adding code to increment/decrement

--- hello-world/HelloWorld/Week2_IncrementDecrement/Week2_IncrementDecrement.cpp
+++ hello-world/HelloWorld/Week2_IncrementDecrement/Week2_IncrementDecrement.cpp
@@ -2,10 +2,41 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Initialize the number.
+    int num = 1;
+
+    /* Each of these blocks will demonstrate increment and decrement results on different sides of the variable.
+    C++ lets you put increment and decrement before the variable so its value is changed before using it.
+    So below let's start.  Most of this code is copy and pasted with different maths on the second line. */
+
+    cout << "num = " << num << endl;
+    cout << "Increment Operator before num = " << ++num << endl;
+    cout << "num after = " << num << endl;
+
+    //reset the number before doing maths again.
+    num = 1;
+
+    cout << "num = " << num << endl;
+    cout << "Increment Operator after num = " << num++ << endl;
+    cout << "num after = " << num << endl;
+
+    num = 1;
+
+    cout << "num = " << num << endl;
+    cout << "Decrement Operator before num = " << --num << endl;
+    cout << "num after = " << num << endl;
+
+    num = 1;
+
+    cout << "num = " << num << endl;
+    cout << "Increment Operator after num = " << num-- << endl;
+    cout << "num after = " << num << endl;
+
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
