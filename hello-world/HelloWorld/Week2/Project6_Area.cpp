70342dac1ceca3fbbcfcfaa070294e17d7e54bce
Colin-J-Wood <w012764k@student.staffs.ac.uk>
2021-10-13 12:44:02 +0100

    fix solution directories, names, and write pi

--- hello-world/HelloWorld/Week2/Project6_Area.cpp
+++ hello-world/HelloWorld/Week2/Project6_Area.cpp
@@ -1,14 +1,14 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	//there's no such thing as a negative side length of an object.
-	unsigned int width, height;
-	width = 15;
-	height = 25;
-
-	unsigned int area = width * height;
-
-	cout << "The area of a " << width << " by " << height << " rectangle is " << area << endl;
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	//there's no such thing as a negative side length of an object.
+//	unsigned int width, height;
+//	width = 15;
+//	height = 25;
+//
+//	unsigned int area = width * height;
+//
+//	cout << "The area of a " << width << " by " << height << " rectangle is " << area << endl;
+//}
