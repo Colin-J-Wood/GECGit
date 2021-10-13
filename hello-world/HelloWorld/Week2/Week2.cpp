9b1f740617afdd0e9a80f21a6991317b9d99d888
Colin-J-Wood <w012764k@student.staffs.ac.uk>
2021-10-13 11:26:52 +0100

    add code showing size of variables in bytes

--- hello-world/HelloWorld/Week2/Week2.cpp
+++ hello-world/HelloWorld/Week2/Week2.cpp
@@ -5,7 +5,15 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Size of char: " << sizeof(char) << " bytes" << std::endl;
+    std::cout << "Size of int: " << sizeof(int) << " bytes" << std::endl;
+    std::cout << "Size of short int: " << sizeof(short int) << " bytes" << std::endl;
+    std::cout << "Size of long int: " << sizeof(long int) << " bytes" << std::endl;
+    std::cout << "Size of float: " << sizeof(float) << " bytes" << std::endl;
+    std::cout << "Size of double: " << sizeof(double) << " bytes" << std::endl;
+    std::cout << "Size of wchar_t: " << sizeof(wchar_t) << " bytes" << std::endl;
+    
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
