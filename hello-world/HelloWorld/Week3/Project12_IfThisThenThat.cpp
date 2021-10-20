55d2acf335839f4cd4e2948585e234d852945d64
Colin-J-Wood <w012764k@student.staffs.ac.uk>
2021-10-20 12:17:12 +0100

    Discount

--- hello-world/HelloWorld/Week3/Project12_IfThisThenThat.cpp
+++ hello-world/HelloWorld/Week3/Project12_IfThisThenThat.cpp
@@ -1,55 +1,55 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	//valids;
-	int rain = 1;
-	int snow = 2;
-	//user inputs;
-	int weather;
-	int temp;
-
-	//get weather.
-	cout << "Please tell me if it's raining with '1' or snowing with '2'." << endl;
-	cin >> weather;
-
-	//get temperature.
-	cout << "Please tell the temperature in whole number Celsius." << endl;
-	cin >> temp;
-
-	//output result.
-	if (weather == 1)
-	{
-		if (temp > 15)
-		{
-			cout << "Wear a light raincoat." << endl;
-		}
-		else
-		{
-			cout << "Wear a thick coat." << endl;
-		}
-	}
-	else if (weather == 2)
-	{
-		if (temp > 5)
-		{
-			cout << "Wear something warm." << endl;
-		}
-		else if (temp > 0)
-		{
-			cout << "Wrap up well." << endl;
-		}
-		else
-		{
-			cout << "Please stay home." << endl;
-		}
-	}
-	else
-	{
-		//I was not instructed to write any handling for temperatures in the event of clear weather.
-		cout << "It's neither raining nor snowing.  Have a nice day!" << endl;
-	}
-
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	//valids;
+//	int rain = 1;
+//	int snow = 2;
+//	//user inputs;
+//	int weather;
+//	int temp;
+//
+//	//get weather.
+//	cout << "Please tell me if it's raining with '1' or snowing with '2'." << endl;
+//	cin >> weather;
+//
+//	//get temperature.
+//	cout << "Please tell the temperature in whole number Celsius." << endl;
+//	cin >> temp;
+//
+//	//output result.
+//	if (weather == 1)
+//	{
+//		if (temp > 15)
+//		{
+//			cout << "Wear a light raincoat." << endl;
+//		}
+//		else
+//		{
+//			cout << "Wear a thick coat." << endl;
+//		}
+//	}
+//	else if (weather == 2)
+//	{
+//		if (temp > 5)
+//		{
+//			cout << "Wear something warm." << endl;
+//		}
+//		else if (temp > 0)
+//		{
+//			cout << "Wrap up well." << endl;
+//		}
+//		else
+//		{
+//			cout << "Please stay home." << endl;
+//		}
+//	}
+//	else
+//	{
+//		//I was not instructed to write any handling for temperatures in the event of clear weather.
+//		cout << "It's neither raining nor snowing.  Have a nice day!" << endl;
+//	}
+//
+//	return 0;
+//}
