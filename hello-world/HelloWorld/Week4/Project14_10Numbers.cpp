9b807a201aad39d20bdedc9e4a11a69cb9c377bd
Colin-J-Wood <w012764k@student.staffs.ac.uk>
2021-10-27 11:50:27 +0100

    Fix Ten Numbers
    allow it to count amount of inputs and calculate mean properly.

--- hello-world/HelloWorld/Week4/Project14_10Numbers.cpp
+++ hello-world/HelloWorld/Week4/Project14_10Numbers.cpp
@@ -6,6 +6,7 @@ int main()
     float inputNum;
     float sumNum = 0.0f; //will also store initialNumber
     float meanNum = 0.0f; //will also store initialNumber
+    int counter = 0;
 
     do
     {
@@ -15,18 +16,23 @@ int main()
             cin >> inputNum;  //needs to be done so 0 can be checked.
             sumNum = inputNum;
             meanNum = inputNum;
+            counter++;
         }
         else
         {
             //stuck this in an else so inputNum is checked before getting the second number.
             cout << "Input the next number:" << endl;
             cin >> inputNum;
-            
-            sumNum = sumNum + inputNum;
-            meanNum = (meanNum + inputNum) / 2;
+
+            if (inputNum != 0) counter++; //don't increase counter if 0 so the final result is a copy of the previous
+            sumNum = sumNum + inputNum; //sum num will end up already having all of the numbers added together, which is the first step of mean calculation anyway
+            meanNum = sumNum / counter;
 
             cout << "New sum: " << sumNum << endl;
             cout << "New mean: " << meanNum << endl;
+
         }
     } while (inputNum != 0);
+
+    return 0;
 }
