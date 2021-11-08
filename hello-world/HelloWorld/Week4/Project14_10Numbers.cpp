c70a6eac44e3ca2ec543bd88202f66698a948e3d
Kaleidio <christhefennec@gmail.com>
2021-11-08 13:17:50 +0000

    Fix loop letting exit too early

--- hello-world/HelloWorld/Week4/Project14_10Numbers.cpp
+++ hello-world/HelloWorld/Week4/Project14_10Numbers.cpp
@@ -1,38 +1,41 @@
-//#include <iostream>
-//using namespace std;
-//
-//int main()
-//{
-//    float inputNum;
-//    float sumNum = 0.0f; //will also store initialNumber
-//    float meanNum = 0.0f; //will also store initialNumber
-//    int counter = 0;
-//
-//    do
-//    {
-//        if (sumNum == 0.0f) //haven't started the loop before, input the initial num
-//        {
-//            cout << "Input the first number:" << endl;
-//            cin >> inputNum;  //needs to be done so 0 can be checked.
-//            sumNum = inputNum;
-//            meanNum = inputNum;
-//            counter++;
-//        }
-//        else
-//        {
-//            //stuck this in an else so inputNum is checked before getting the second number.
-//            cout << "Input the next number:" << endl;
-//            cin >> inputNum;
-//
-//            if (inputNum != 0) counter++; //don't increase counter if 0 so the final result is a copy of the previous
-//            sumNum = sumNum + inputNum; //sum num will end up already having all of the numbers added together, which is the first step of mean calculation anyway
-//            meanNum = sumNum / counter;
-//
-//            cout << "New sum: " << sumNum << endl;
-//            cout << "New mean: " << meanNum << endl;
-//
-//        }
-//    } while (inputNum != 0);
-//
-//    return 0;
-//}
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float inputNum;
+    float sumNum = 0.0f; //will also store initialNumber
+    float meanNum = 0.0f; //will also store initialNumber
+    int counter = 2;
+
+    cout << "Input the first number:" << endl;
+    cin >> inputNum;  //needs to be done so 0 can be checked.
+    sumNum = inputNum;
+    meanNum = inputNum;
+
+    //get second number immediately
+    cout << "Input the next number:" << endl;
+    cin >> inputNum;
+
+    sumNum = sumNum + inputNum; //sum num will end up already having all of the numbers added together, which is the first step of mean calculation anyway
+    meanNum = sumNum / counter;
+
+    cout << "New sum: " << sumNum << endl;
+    cout << "New mean: " << meanNum << endl;
+
+    do
+    {
+        //stuck this in an else so inputNum is checked before getting the second number.
+        cout << "Input the next number:" << endl;
+        cin >> inputNum;
+
+        if (inputNum != 0) counter++; //don't increase counter if 0 so the final result is a copy of the previous
+        sumNum = sumNum + inputNum; //sum num will end up already having all of the numbers added together, which is the first step of mean calculation anyway
+        meanNum = sumNum / counter;
+
+        cout << "New sum: " << sumNum << endl;
+        cout << "New mean: " << meanNum << endl;
+    } while (inputNum != 0);
+
+    return 0;
+}
--- hello-world/HelloWorld/Week4/Project15_StarryOutput.cpp
+++ hello-world/HelloWorld/Week4/Project15_StarryOutput.cpp
@@ -1,62 +1,62 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	int num;
-	char check = 'y';
-
-	do
-	{
-		cout << "Input a number between 1 and 10" << endl;
-		cin >> num;
-
-		if ((num >= 1) && (num <= 10))
-		{
-			for (int row = 0; row < num; row++)
-			{
-				//print out one star at a time so we can do this dynamically.
-				//print out as many stars as there are rows, cascading.
-				for (int star = 0; star < row + 1; star++)
-				{
-					cout << "* ";
-				}
-
-				cout << endl;
-			}
-
-			bool queryContinue = false; //speed up this check
-			do
-			{
-				cout << "Would you like to have another go? (y/n)" << endl;
-				cin >> check;
-
-				if ((check == 'Y') || (check == 'N'))
-				{
-					cout << "Caps detected." << endl;
-				}
-				else if ((check != 'y') && (check != 'n'))
-				{
-					cout << "Invalid response." << endl;
-				}
-				else
-				{
-					queryContinue = true;
-				}
-
-			} while (queryContinue == false);
-		}
-		else
-		{
-			cout << "That number's out of range." << endl;
-
-			//we can let the loop check its condition here, as we haven't changed check at all
-			//so it should always end up succeeding and causing a loop back up to inputting a number.
-		}
-	} while (check == 'y');
-
-	cout << "Goodbye" << endl;
-	cin.ignore();
-
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int num;
+//	char check = 'y';
+//
+//	do
+//	{
+//		cout << "Input a number between 1 and 10" << endl;
+//		cin >> num;
+//
+//		if ((num >= 1) && (num <= 10))
+//		{
+//			for (int row = 0; row < num; row++)
+//			{
+//				//print out one star at a time so we can do this dynamically.
+//				//print out as many stars as there are rows, cascading.
+//				for (int star = 0; star < row + 1; star++)
+//				{
+//					cout << "* ";
+//				}
+//
+//				cout << endl;
+//			}
+//
+//			bool queryContinue = false; //speed up this check
+//			do
+//			{
+//				cout << "Would you like to have another go? (y/n)" << endl;
+//				cin >> check;
+//
+//				if ((check == 'Y') || (check == 'N'))
+//				{
+//					cout << "Caps detected." << endl;
+//				}
+//				else if ((check != 'y') && (check != 'n'))
+//				{
+//					cout << "Invalid response." << endl;
+//				}
+//				else
+//				{
+//					queryContinue = true;
+//				}
+//
+//			} while (queryContinue == false);
+//		}
+//		else
+//		{
+//			cout << "That number's out of range." << endl;
+//
+//			//we can let the loop check its condition here, as we haven't changed check at all
+//			//so it should always end up succeeding and causing a loop back up to inputting a number.
+//		}
+//	} while (check == 'y');
+//
+//	cout << "Goodbye" << endl;
+//	cin.ignore();
+//
+//	return 0;
+//}
