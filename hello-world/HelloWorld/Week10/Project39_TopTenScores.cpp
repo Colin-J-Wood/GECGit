2d18562ab62cb9b448542e413f530192e25b7edb
Kaleidio <w012764k@student.staffs.ac.uk>
2021-12-08 16:18:03 +0000

    some more difficulty...

--- hello-world/HelloWorld/Week10/Project39_TopTenScores.cpp
+++ hello-world/HelloWorld/Week10/Project39_TopTenScores.cpp
@@ -2,27 +2,34 @@
 #include <fstream>
 #include <string>
 #include <iomanip>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-void inputScore(string fileName);
-void outputScore(string fileName);
+void handleScore(string fileName, bool inputMode);
 
 string fileName_global = "scores.txt";
 
 struct score
 {
 	string name;
-	int score;
+	int scoreValue = 0;
 };
 
 int main()
 {
+	//create the file if it hasn't already.
+	ofstream newFile("scores.txt");
+	newFile.close();
+
 	bool cont = true;
 
 	while (cont)
 	{
 		int choice = 0;
+
+		setw(0);
 
 		cout << "Input a choice:" << endl;
 		cout << "1. Enter a score" << endl;
@@ -33,11 +40,11 @@ int main()
 		switch (choice)
 		{
 			case 1:
-				inputScore(fileName_global);
+				handleScore(fileName_global, true);
 
 				break;
 			case 2:
-				outputScore(fileName_global);
+				handleScore(fileName_global, false);
 
 				break;
 			case 3:
@@ -51,15 +58,96 @@ int main()
 		}
 	}
 
+	cout << "Goodbye!" << endl;
+
 	return 0;
 }
 
-void inputScore(string fileName)
+void handleScore(string fileName, bool inputMode)
 {
+	fstream file;
+	vector<score> scoreList;
+	score nextScore;
+	string textToCopy;
+	int index = 0;
 
-}
+	file.open(fileName, ios::in | ios::out);
 
-void outputScore(string fileName)
-{
+	if (!file)
+	{
+		cout << "Failure opening file." << endl;
+		return;
+	}
 
+	while (!file.eof())
+	{
+		index++;
+		switch (index)
+		{
+		case 1:
+			getline(file, textToCopy);
+
+			nextScore.name = textToCopy;
+
+			break;
+		case 2:
+			file >> nextScore.scoreValue;
+
+			scoreList.push_back(nextScore);
+
+			file.ignore();
+			index = 0;
+			break;
+		}
+	}
+
+	if (scoreList.size() == 0)
+	{
+		//make a new file with all ten slots filled
+		for (int i = 0; i < 10; i++)
+		{
+			nextScore.name = "Default";
+			nextScore.scoreValue = 10000 - (1000 * i);
+
+			scoreList.push_back(nextScore);
+		}
+
+		//having difficulty getting this working too...
+		for (score i : scoreList)
+		{
+			file << i.name << endl;
+			file << i.scoreValue << endl;
+		}
+	}
+
+	//having difficulty getting this one working...
+	if (inputMode)
+	{
+		cout << "Input a name: ";
+		cin >> textToCopy;
+
+		nextScore.name = textToCopy;
+
+		cout << "Input a score: ";
+		cin >> nextScore.scoreValue;
+		cin.ignore();
+
+		file.clear();
+
+		for (score i : scoreList)
+		{
+			file << i.name << endl;
+			file << i.scoreValue << endl;
+		}
+	}
+
+	for (score i : scoreList)
+	{
+		setw(10);
+		cout << i.name;
+		cout << i.scoreValue << endl;
+		setw(0);
+	}
+
+	file.close();
 }
