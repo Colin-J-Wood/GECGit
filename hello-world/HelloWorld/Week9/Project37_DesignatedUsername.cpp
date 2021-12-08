7cf859cf815407987d87fea516aa217f67d41407
Colin-J-Wood <w012764k@student.staffs.ac.uk>
2021-12-08 12:22:56 +0000

    accounts
    double isn't getting input properly?

--- hello-world/HelloWorld/Week9/Project37_DesignatedUsername.cpp
+++ hello-world/HelloWorld/Week9/Project37_DesignatedUsername.cpp
@@ -1,30 +1,30 @@
-#include <iostream>
-#include <ctime>
-#include <string>
-
-using namespace std;
-
-void makeNewName(string& ref_name);
-
-int main()
-{
-	string name;
-
-	cout << "Please enter a new name: " << endl;
-	getline(cin, name);
-
-	cout << "That name is taken." << endl;
-	makeNewName(name);
-	cout << "Here's a modified version of that name:" << endl;
-	cout << name;
-
-	return 0;
-}
-
-void makeNewName(string& ref_name)
-{
-	srand(time(NULL));
-	int num = rand();
-
-	ref_name = ref_name + to_string(num);
-}
+//#include <iostream>
+//#include <ctime>
+//#include <string>
+//
+//using namespace std;
+//
+//void makeNewName(string& ref_name);
+//
+//int main()
+//{
+//	string name;
+//
+//	cout << "Please enter a new name: " << endl;
+//	getline(cin, name);
+//
+//	cout << "That name is taken." << endl;
+//	makeNewName(name);
+//	cout << "Here's a modified version of that name:" << endl;
+//	cout << name;
+//
+//	return 0;
+//}
+//
+//void makeNewName(string& ref_name)
+//{
+//	srand(time(NULL));
+//	int num = rand();
+//
+//	ref_name = ref_name + to_string(num);
+//}
